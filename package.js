3753ab11e7e94dbc3db8d36b14564e4025323979
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-02-07 21:50:16 +0900

    add jQuery-validation 1.11.0

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+    summary : "jzaefferer/jquery-validation for Meteor smart package"
+});
+
+Package.on_use(function(api, where) {
+    where = where || [ 'client', 'server' ];
+    api.use('jquery', where);
+
+    api.add_files('lib/jquery.validate.js', where);
+    api.add_files('lib/additional-method.js', where);
+    api.add_files('lib/lib/jquery.form.js', where);
+    api.add_files('lib/lib/jquery.mockjax.js', where);
+    api.add_files('lib/localization/messages_ja.js', where);
+});
